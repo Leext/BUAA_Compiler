6c722039e9962db006aba4998b25c3c02f6ddf3a
lee <leext@buaa.edu.cn>
2017-11-29 17:48:18 +0800

    修改了Quad的结构

--- Compiler/Compiler/Quad.cpp
+++ Compiler/Compiler/Quad.cpp
@@ -1,3 +1,44 @@
 #include "StdAfx.h"
 #include "Quad.h"
+string Operator::toString()
+{
+	string r;
+	r += "(";
+	r += s1->toString();
+	switch (opcode)
+	{
+	case Op_ADD:
+		r += "+";
+		break;
+	case Op_SUB:
+		r += "-";
+		break;
+	case Op_DIV:
+		r += "/";
+		break;
+	case Op_MULT:
+		r += "*";
+		break;
+	}
+	r += s2->toString();
+	return std::move(r);
+}
 
+string Constant::toString()
+{
+	std::stringstream ss;
+	if (type == T_INT)
+		ss << val;
+	else ss << (char)val;
+	return std::move(ss.str());
+}
+
+string Var::toString()
+{
+	return name;
+}
+
+string Array::toString()
+{
+	return name + "[" + offset->toString() + "]";
+}
--- Compiler/Compiler/Quad.h
+++ Compiler/Compiler/Quad.h
@@ -3,16 +3,26 @@
 #include "SymbolTable.h"
 using std::string;
 using std::vector;
-
-
+enum Opcode
+{
+	Op_CONST,
+	Op_VAR,
+	Op_LABEL,
+	Op_GOTO,
+	Op_FUNCALL,
+	Op_VOIDCALL,
+	Op_ADD,
+	Op_SUB,
+	Op_DIV,
+	Op_MULT,
+	Op_ASSIGN,
+	Op_ARRAY,
+	Op_RETURN
+};
 class Quad
 {
 public:
-	enum Opcode
-	{
-		CONST, VAR, LABEL, GOTO, FUNCALL, VOIDCALL, ASSIGN, ARRAY,
-		RETURN
-	};
+
 	Quad(Opcode opcode) : opcode(opcode) {}
 	const Opcode opcode;
 	virtual string toString() = 0;
@@ -21,7 +31,8 @@ public:
 class Value : public Quad
 {
 public:
-	Value(Opcode opcode) : Quad(opcode) {}
+	const Type type;
+	Value(Opcode opcode, Type type) : Quad(opcode), type(type) {}
 };
 
 class Instruction : public Quad
@@ -33,25 +44,25 @@ public:
 class Label : public Instruction
 {
 public:
-	Label() : Instruction(LABEL) {}
-	BasicBlock* controller;
+	Label() : Instruction(Op_LABEL) {}
+	BasicBlock *controller;
 };
 
 class Constant : public Value
 {
 public:
-	Constant(int intVal) : Value(CONST), val(intVal), type(T_INT) {}
-	Constant(char charVal) : Value(CONST), val(charVal), type(T_CHAR) {}
+	Constant(int intVal) : Value(Op_CONST, T_INT), val(intVal) {}
+	Constant(char charVal) : Value(Op_CONST, T_CHAR), val(charVal) {}
 	const int val;
-	const Type type;
+	virtual string toString();
 };
 
 class Var : public Value
 {
 public:
 	const string name;
-	const Type type;
-	Var(const string& name, Type type) : Value(VAR), name(name), type(type) {}
+	Var(const string &name, Type type) : Value(Op_VAR, type), name(name) {}
+	virtual string toString();
 };
 
 class Operator : public Value
@@ -59,64 +70,64 @@ class Operator : public Value
 public:
 	Value *s1;
 	Value *s2;
-	Operator(Opcode opcode, Value* const s1, Value* const s2) : Value(opcode), s1(s1), s2(s2) {}
-
+	Operator(Opcode opcode, Type type, Value *const s1, Value *const s2) : Value(opcode, type), s1(s1), s2(s2) {}
+	virtual string toString();
 };
 
 class Goto : public Instruction
 {
 public:
-	const Label* label;
-	Goto(Label* const label) : Instruction(GOTO), label(label) {}
+	const Label *label;
+	Goto(Label *const label) : Instruction(Op_GOTO), label(label) {}
 };
 
 class CmpBr : public Instruction
 {
 public:
-	const Label* label;
+	const Label *label;
 	Value *s1;
 	Value *s2;
-	CmpBr(Opcode opcode, Value* const s1, Value* const s2, Label* const label) : Instruction(opcode), label(label), s1(s1), s2(s2) {}
+	CmpBr(Opcode opcode, Value *const s1, Value *const s2, Label *const label) : Instruction(opcode), label(label), s1(s1), s2(s2) {}
 };
 
 class FunctCall : public Value
 {
 public:
-	vector<Value*> args;
+	vector<Value *> args;
 	string name;
-	FunctCall(const string& name, vector<Value*> args) :name(name), args(std::move(args)), Value(FUNCALL) {}
+	FunctCall(const string &name, Type type, vector<Value *> args) : name(name), args(std::move(args)), Value(Op_FUNCALL, type) {}
 };
 
 class VoidCall : public Instruction
 {
 public:
-	vector<Value*> args;
+	vector<Value *> args;
 	string name;
-	VoidCall(const string& name, vector<Value*> args) :name(name), args(std::move(args)), Instruction(VOIDCALL) {}
+	VoidCall(const string &name, vector<Value *> args) : name(name), args(std::move(args)), Instruction(Op_VOIDCALL) {}
 };
 
 class Array : public Value
 {
 public:
-	Value* offset;
+	Value *offset;
 	const string name;
-	const Type type;
-	Array(Value * const offset, const string& name,Type type) :offset(offset), name(name), type(type), Value(ARRAY) {}
+	Array(Value *const offset, const string &name, Type type) : offset(offset), name(name), Value(Op_ARRAY, type) {}
+	virtual string toString();
 };
 
 class Assign : public Instruction
 {
 public:
-	Value* var;
+	Value *var;
 	Value *s1;
-	Assign(Var * const var, Value * const s1) :var(var), s1(s1), Instruction(ASSIGN) {}
-	Assign(Array * const var, Value * const s1) :var(var), s1(s1), Instruction(ASSIGN) {}
+	Assign(Var *const var, Value *const s1) : var(var), s1(s1), Instruction(Op_ASSIGN) {}
+	Assign(Array *const var, Value *const s1) : var(var), s1(s1), Instruction(Op_ASSIGN) {}
 };
 
 class Return : public Instruction
 {
 public:
-	Value* ret;
-	Return(Value * const var) : ret(var), Instruction(RETURN) {}
-	Return() :ret(nullptr), Instruction(RETURN) {}
+	Value *ret;
+	Return(Value *const var) : ret(var), Instruction(Op_RETURN) {}
+	Return() : ret(nullptr), Instruction(Op_RETURN) {}
 };
