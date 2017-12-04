7215337858090d24dd83a712b42f52d52c41b8a4
lee <leext@buaa.edu.cn>
2017-12-04 20:31:34 +0800

    将所有toString改为const，完成了部分toString

--- Compiler/Compiler/Quad.cpp
+++ Compiler/Compiler/Quad.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
+#include "BasicBlock.h"
 #include "Quad.h"
-string Operator::toString()
+string Operator::toString() const
 {
 	string r;
 	r += "(";
@@ -25,7 +26,7 @@ string Operator::toString()
 	return std::move(r);
 }
 
-string Constant::toString()
+string Constant::toString() const
 {
 	std::stringstream ss;
 	if (type == T_INT)
@@ -35,67 +36,88 @@ string Constant::toString()
 	return std::move(ss.str());
 }
 
-string Var::toString()
+string Var::toString() const
 {
 	return name;
 }
 
-string Array::toString()
+string Array::toString() const
 {
 	return name + "[" + offset->toString() + "]";
 }
 
-string FunctCall::toString()
+string FunctCall::toString() const
 {
 	string ret = name;
 	if (args.size())
 	{
-		ret += "(";
+		ret += '(';
 		for (auto i = args.begin(); i != args.end(); i++)
 			ret += (*i)->toString() + ", ";
 		ret.pop_back();
 		ret.pop_back();
-		ret += ")";
+		ret += ')';
 	}
 	return ret;
 }
 
-string Label::toString()
+string Label::toString() const
 {
-	return string();
+	return string("block " + std::to_string((long long)(controller->id)));
 }
 
-string CmpBr::toString()
+string CmpBr::toString() const
 {
-	return string();
+	string ret = "compare&branch";
+	return ret;
 }
 
-string Goto::toString()
+string Goto::toString() const
 {
-	return string();
+	return string("goto " + label->toString());
 }
 
-string VoidCall::toString()
+string VoidCall::toString() const
 {
-	return string();
+	string ret = name;
+	ret += '(';
+	if (args.size())
+	{
+		for (auto i = args.begin(); i != args.end(); i++)
+			ret += (*i)->toString() + ", ";
+		ret.pop_back();
+		ret.back() = ')';
+	}
+	return ret;
 }
 
-string Assign::toString()
+string Assign::toString() const
 {
-	return string();
+	return string(var->toString() + " = " + s1->toString());
 }
 
-string Return::toString()
+string Return::toString() const
 {
-	return string();
+	return string("return" + (ret == nullptr ? "" : ret->toString()));
 }
 
-string Scanf::toString()
+string Scanf::toString() const
 {
-	return string();
+	string ret = "scanf(";
+	for (auto i = args.begin(); i != args.end(); i++)
+		ret += (*i)->toString() + ", ";
+	ret.pop_back();
+	ret.back() = ')';
+	return ret;
 }
 
-string Printf::toString()
+string Printf::toString() const
 {
-	return string();
+	string ret = "printf(";
+	if (strIndex >= 0)
+		ret += "str " + std::to_string((long long)strIndex) + " , ";
+	if (value != nullptr)
+		ret += value->toString();
+	ret += ')';
+	return ret;
 }
--- Compiler/Compiler/Quad.h
+++ Compiler/Compiler/Quad.h
@@ -24,144 +24,150 @@ enum Opcode
 	Op_SCANF,
 	Op_PRINTF,
 	Op_BEQ,
+	Op_BEQZ,
 	Op_BNE,
+	Op_BLE,
+	Op_BGE,
+	Op_BGT,
+	Op_BLT,
 };
 class Quad
 {
-  public:
+public:
 	Quad(Opcode opcode) : opcode(opcode) {}
 	const Opcode opcode;
-	virtual string toString() = 0;
+	virtual string toString() const = 0;
 };
 
 class Value : public Quad
 {
-  public:
+public:
 	const Type type;
 	Value(Opcode opcode, Type type) : Quad(opcode), type(type) {}
 };
 
 class Instruction : public Quad
 {
-  public:
+public:
 	Instruction(Opcode) : Quad(opcode) {}
 };
 
 class Label : public Instruction
 {
-  public:
+public:
 	Label() : Instruction(Op_LABEL) {}
 	Label(BasicBlock *c) : Instruction(Op_LABEL), controller(c) {}
 	BasicBlock *controller;
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Constant : public Value
 {
-  public:
+public:
 	Constant(int intVal) : Value(Op_CONST, T_INT), val(intVal) {}
 	Constant(char charVal) : Value(Op_CONST, T_CHAR), val(charVal) {}
 	const int val;
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Var : public Value
 {
-  public:
+public:
 	const string name;
 	Var(const string &name, Type type) : Value(Op_VAR, type), name(name) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Operator : public Value
 {
-  public:
+public:
 	Value *s1;
 	Value *s2;
 	Operator(Opcode opcode, Type type, Value *const s1, Value *const s2) : Value(opcode, type), s1(s1), s2(s2) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Goto : public Instruction
 {
-  public:
+public:
 	const Label *label;
 	Goto(Label *const label) : Instruction(Op_GOTO), label(label) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class CmpBr : public Instruction
 {
-  public:
+public:
 	const Label *label;
 	Value *s1;
 	Value *s2;
 	CmpBr(Opcode opcode, Value *const s1, Value *const s2, Label *const label) : Instruction(opcode), label(label), s1(s1), s2(s2) {}
-	virtual string toString();
+	CmpBr(Opcode opcode, Value *const s1, Label *const label) : Instruction(opcode), label(label), s1(s1), s2(nullptr) {}
+	virtual string toString() const;
 };
 
 class FunctCall : public Value
 {
-  public:
+public:
 	vector<Value *> args;
 	string name;
 	FunctCall(const string &name, Type type, vector<Value *> args) : name(name), args(std::move(args)), Value(Op_FUNCALL, type) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class VoidCall : public Instruction
 {
-  public:
+public:
 	vector<Value *> args;
 	string name;
 	VoidCall(const string &name, vector<Value *> args) : name(name), args(std::move(args)), Instruction(Op_VOIDCALL) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Array : public Value
 {
-  public:
+public:
 	Value *offset;
 	const string name;
 	Array(Value *const offset, const string &name, Type type) : offset(offset), name(name), Value(Op_ARRAY, type) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Assign : public Instruction
 {
-  public:
+public:
 	Value *var;
 	Value *s1;
 	Assign(Var *const var, Value *const s1) : var(var), s1(s1), Instruction(Op_ASSIGN) {}
 	Assign(Array *const var, Value *const s1) : var(var), s1(s1), Instruction(Op_ASSIGN) {}
 	Assign(Value *const var, Value *const s1) : var(var), s1(s1), Instruction(Op_ASSIGN) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Return : public Instruction
 {
-  public:
+public:
 	Value *ret;
 	Return(Value *const var) : ret(var), Instruction(Op_RETURN) {}
 	Return() : ret(nullptr), Instruction(Op_RETURN) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Scanf : public Instruction
 {
-  public:
+public:
 	vector<Var *> args;
 	Scanf(vector<Var *> args) : args(std::move(args)), Instruction(Op_SCANF) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 
 class Printf : public Instruction
 {
-  public:
+public:
 	Value *value;
 	int strIndex;
 	Printf(int strIndex, Value *value = nullptr) : strIndex(strIndex), value(value), Instruction(Op_PRINTF) {}
 	Printf(Value *value) : strIndex(-1), value(value), Instruction(Op_PRINTF) {}
-	virtual string toString();
+	virtual string toString() const;
 };
 #endif // !__QUAD_H_
